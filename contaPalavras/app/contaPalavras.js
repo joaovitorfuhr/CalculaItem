3bef566a56e315a703d3312d3b33c392825d1a7d
joaovitorfuhr <joaovf.jvf.12@gmail.com>
2017-05-16 21:24:27 -0300

    app funcionando

--- contaPalavras/app/contaPalavras.js
+++ contaPalavras/app/contaPalavras.js
@@ -1,6 +1,3 @@
 
 function contaPalavras(){
-	var texto = document.ex01.campo.value;
-	var palavra = texto.split([COLOR="Red"][B]' '[/B][/COLOR]);
-
-	document.getElementById('alvo').innerHTML = [COLOR="red"][B]palavra.length[/B][/COLOR];
+"frase teste".split(' ').length
